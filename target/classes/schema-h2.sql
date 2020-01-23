e9639c7aae98675f333077e588d4e25c15dba238
webadgerdev <25802580>
2020-01-23 00:12:10 -0300

    added /target folder

---
+++ target/classes/schema-h2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS tProduto(
+    id VARCHAR(30) PRIMARY KEY,
+    product VARCHAR(20), 
+    quantity INT, 
+    price VARCHAR(20), 
+    type VARCHAR(20), 
+    industry VARCHAR(100), 
+    origin VARCHAR(20)
+);
